27155a3875241008d7742f356e7aa307a2c2e37c
Mickabtech <mickabtech@gmail.com>
2024-11-26 02:18:25 -0700

    get all news and delete news features

--- controllers/NewsController.js
+++ controllers/NewsController.js
@@ -24,3 +24,43 @@ const createNews = async (req, res) => {
       res.status(400).json({ message: error.message });
     }
   };
+
+
+  // Get all news
+const getNews = async (req, res) => {
+    try {
+      const news = await News.find({});
+      res.status(200).json({
+        Message: "News found",
+        Iwe: news,
+        NumberOfBooks: news.length,
+      });
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  };
+
+  // Delete a book
+const deleteNews = async (req, res) => {
+    try {
+      const { id } = req.params;
+  
+      const news = await News.findByIdAndDelete(id);
+  
+      if (!news) {
+        return res.status(404).json({ message: "News not found" });
+      }
+  
+      res.status(200).json({ message: "News successfully deleted" });
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  };
+  
+
+
+  module.exports = {
+    createNews,
+    getNews,
+    deleteNews,
+  };
